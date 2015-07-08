279f3254aec983f41ea2df00a7189c2e26b04b05
Matthew FitzPatrick <kaamoss@gmail.com>
2015-07-08 12:09:39 -0700

    Changes to read doctrine 2 entity config from annotations instead of XML. Also adding support for redis via PRedis for doctrine 2 caching layer.

--- src/Doctrine2Bridge/Doctrine2BridgeServiceProvider.php
+++ src/Doctrine2Bridge/Doctrine2BridgeServiceProvider.php
@@ -5,6 +5,8 @@ namespace Doctrine2Bridge;
 use Auth;
 use Config;
 
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Cache\Cache;
@@ -24,165 +26,178 @@ use \Doctrine2Bridge\Support\Repository as D2Repository;
  */
 class Doctrine2BridgeServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    /**
-     * Have we been configured?
-     */
-    private $configured = true;
+  /**
+   * Have we been configured?
+   */
+  private $configured = true;
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot( \Doctrine\Common\Cache\Cache $d2cache )
-    {
-        // handle publishing of config file:
-        $this->handleConfigs();
+  /**
+   * Bootstrap the application events.
+   *
+   * @return void
+   */
+  public function boot( \Doctrine\Common\Cache\Cache $d2cache )
+  {
+    // handle publishing of config file:
+    $this->handleConfigs();
 
-        if( !$this->configured ) {
-            if( isset( $_SERVER['argv'][1] ) && $_SERVER['argv'][1] != 'vendor:publish' )
-                echo "You must pubish the configuration files first: artisan vendor:publish\n";
-            return;
-        }
-
-        $d2em = $this->app->make( \Doctrine\ORM\EntityManagerInterface::class );
-        $d2em->getConfiguration()->setMetadataCacheImpl( $d2cache );
-        $d2em->getConfiguration()->setQueryCacheImpl( $d2cache );
-        $d2em->getConnection()->getConfiguration()->setResultCacheImpl( $d2cache );
-
-        if( Config::get( 'd2bdoctrine.sqllogger.enabled' ) )
-            $this->attachLogger( $d2em );
+    if( !$this->configured ) {
+      if( isset( $_SERVER['argv'][1] ) && $_SERVER['argv'][1] != 'vendor:publish' )
+        echo "You must pubish the configuration files first: artisan vendor:publish\n";
+      return;
     }
 
+    $d2em = $this->app->make( \Doctrine\ORM\EntityManagerInterface::class );
+    $d2em->getConfiguration()->setMetadataCacheImpl( $d2cache );
+    $d2em->getConfiguration()->setQueryCacheImpl( $d2cache );
+    $d2em->getConnection()->getConfiguration()->setResultCacheImpl( $d2cache );
+
+    if( Config::get( 'd2bdoctrine.sqllogger.enabled' ) )
+      $this->attachLogger( $d2em );
+
+    if( Config::get( 'd2bdoctrine.auth.enabled' ) )
+      $this->setupAuth();
+  }
 
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        if( !Config::get( 'd2bdoctrine' ) ) {
-            if( isset( $_SERVER['argv'][1] ) && $_SERVER['argv'][1] != 'vendor:publish' )
-                echo "You must pubish the configuration files first: artisan vendor:publish\n";
-            $this->configured = false;
-            return;
-        }
 
-        $this->registerEntityManager();
-        $this->registerClassMetadataFactory();
-        $this->registerConsoleCommands();
-        $this->registerRepositoryFacade();
-        $this->registerFacades();
-
-        if( Config::get( 'd2bdoctrine.auth.enabled' ) )
-            $this->setupAuth();
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    if( !Config::get( 'd2bdoctrine' ) ) {
+      if( isset( $_SERVER['argv'][1] ) && $_SERVER['argv'][1] != 'vendor:publish' )
+        echo "You must pubish the configuration files first: artisan vendor:publish\n";
+      $this->configured = false;
+      return;
     }
 
-    /**
-     * The Entity Manager - why we're all here!
-     */
-    private function registerEntityManager()
-    {
-        $this->app->singleton( EntityManagerInterface::class, function( $app ) {
+    $this->registerEntityManager();
+    $this->registerClassMetadataFactory();
+    $this->registerConsoleCommands();
+    $this->registerRepositoryFacade();
+    $this->registerFacades();
+  }
 
-            $dconfig = new \Doctrine\ORM\Configuration;
+  /**
+   * The Entity Manager - why we're all here!
+   */
+  private function registerEntityManager()
+  {
+    $this->app->singleton( EntityManagerInterface::class, function( $app ) {
 
-            $driver = new \Doctrine\ORM\Mapping\Driver\XmlDriver(
-                array( Config::get( 'd2bdoctrine.paths.xml_schema' ) )
-            );
-
-            $dconfig->setMetadataDriverImpl( $driver );
-
-            $dconfig->setProxyDir(                 Config::get( 'd2bdoctrine.paths.proxies'      ) );
-            $dconfig->setProxyNamespace(           Config::get( 'd2bdoctrine.namespaces.proxies' ) );
-            $dconfig->setAutoGenerateProxyClasses( Config::get( 'd2bdoctrine.autogen_proxies'    ) );
-
-            $lconfig = $this->laravelToDoctrineConfigMapper();
-
-            //load prefix listener
-            if( isset($lconfig['prefix']) && $lconfig['prefix'] && $lconfig['prefix'] !== '' ) {
-                $tablePrefix = new TablePrefix( $lconfig['prefix']);
-                $eventManager->addEventListener(Events::loadClassMetadata, $tablePrefix);
-            }
-
-            return EntityManager::create( $lconfig, $dconfig );
-        });
-
-    }
-
-    /**
-     * Register Facades to make developers' lives easier
-     */
-    private function registerFacades()
-    {
-        // Shortcut so developers don't need to add an Alias in app/config/app.php
-        \App::booting( function() {
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-            $loader->alias( 'D2EM', 'Doctrine2Bridge\Support\Facades\Doctrine2' );
-        });
-    }
-
-    /**
-     * Register Laravel console commands
-     */
-    private function registerConsoleCommands()
-    {
-        $this->commands([
-            "\Doctrine2Bridge\Console\Generators\All",
-            "\Doctrine2Bridge\Console\Generators\Entities",
-            "\Doctrine2Bridge\Console\Generators\Proxies",
-            "\Doctrine2Bridge\Console\Generators\Repositories",
-
-            "\Doctrine2Bridge\Console\Schema\Create",
-            "\Doctrine2Bridge\Console\Schema\Drop",
-            "\Doctrine2Bridge\Console\Schema\Update",
-            "\Doctrine2Bridge\Console\Schema\Validate",
-        ]);
-    }
-
-    /**
-     * Metadata Factory - mainly used by schema console commands
-     */
-    private function registerClassMetadataFactory()
-    {
-        $this->app->singleton( ClassMetadataFactory::class, function( $app ) {
-            return $app[EntityManagerInterface::class]->getMetadataFactory();
-        });
-    }
+      $dconfig = new \Doctrine\ORM\Configuration;
 
 
-    private function registerRepositoryFacade()
-    {
-        $this->app->bind( D2Repository::class, function( $app ) {
-            return new D2Repository;
-        });
+      AnnotationRegistry::registerAutoloadNamespace(Config::get( 'd2bdoctrine.namespaces.models' ), Config::get( 'd2bdoctrine.paths.models' ));
+      AnnotationRegistry::registerLoader('class_exists');
 
-        \App::booting( function() {
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-            $loader->alias( 'D2R', 'Doctrine2Bridge\Support\Facades\Doctrine2Repository' );
-        });
+      $reader = new AnnotationReader();
 
-    }
+      $driver = new \Doctrine\ORM\Mapping\Driver\AnnotationDriver(
+          $reader,
+          Config::get( 'd2bdoctrine.paths.models' )
+      );
 
-    /**
-     * Attach Laravel logging to Doctrine for debugging / profiling
-     */
-    private function attachLogger( $d2em )
-    {
-        $logger = new Logger\Laravel;
-        if( Config::has( 'd2bdoctrine.sqllogger.level' ) )
-            $logger->setLevel( Config::get( 'd2bdoctrine.sqllogger.level' ) );
+      /*
+        $driver = new \Doctrine\ORM\Mapping\Driver\XmlDriver(
+            array( Config::get( 'd2bdoctrine.paths.xml_schema' ) )
+        );
+      */
 
-        $d2em->getConnection()->getConfiguration()->setSQLLogger( $logger );
-    }
+      $dconfig->setMetadataDriverImpl( $driver );
 
-    /**
-     * Set up Laravel authentication via Doctrine2 provider
-     */
-    private function setupAuth()
-    {
+      $dconfig->setProxyDir(                 Config::get( 'd2bdoctrine.paths.proxies'      ) );
+      $dconfig->setProxyNamespace(           Config::get( 'd2bdoctrine.namespaces.proxies' ) );
+      $dconfig->setAutoGenerateProxyClasses( Config::get( 'd2bdoctrine.autogen_proxies'    ) );
+
+      $lconfig = $this->laravelToDoctrineConfigMapper();
+
+      //load prefix listener
+      if( isset($lconfig['prefix']) && $lconfig['prefix'] && $lconfig['prefix'] !== '' ) {
+        $tablePrefix = new TablePrefix( $lconfig['prefix']);
+        $eventManager->addEventListener(Events::loadClassMetadata, $tablePrefix);
+      }
+
+      return EntityManager::create( $lconfig, $dconfig );
+    });
+
+  }
+
+  /**
+   * Register Facades to make developers' lives easier
+   */
+  private function registerFacades()
+  {
+    // Shortcut so developers don't need to add an Alias in app/config/app.php
+    \App::booting( function() {
+      $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+      $loader->alias( 'D2EM', 'Doctrine2Bridge\Support\Facades\Doctrine2' );
+    });
+  }
+
+  /**
+   * Register Laravel console commands
+   */
+  private function registerConsoleCommands()
+  {
+    $this->commands([
+        "\Doctrine2Bridge\Console\Generators\All",
+        "\Doctrine2Bridge\Console\Generators\Entities",
+        "\Doctrine2Bridge\Console\Generators\Proxies",
+        "\Doctrine2Bridge\Console\Generators\Repositories",
+
+        "\Doctrine2Bridge\Console\Schema\Create",
+        "\Doctrine2Bridge\Console\Schema\Drop",
+        "\Doctrine2Bridge\Console\Schema\Update",
+        "\Doctrine2Bridge\Console\Schema\Validate",
+    ]);
+  }
+
+  /**
+   * Metadata Factory - mainly used by schema console commands
+   */
+  private function registerClassMetadataFactory()
+  {
+    $this->app->singleton( ClassMetadataFactory::class, function( $app ) {
+      return $app[EntityManagerInterface::class]->getMetadataFactory();
+    });
+  }
+
+
+  private function registerRepositoryFacade()
+  {
+    $this->app->bind( D2Repository::class, function( $app ) {
+      return new D2Repository;
+    });
+
+    \App::booting( function() {
+      $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+      $loader->alias( 'D2R', 'Doctrine2Bridge\Support\Facades\Doctrine2Repository' );
+    });
+
+  }
+
+  /**
+   * Attach Laravel logging to Doctrine for debugging / profiling
+   */
+  private function attachLogger( $d2em )
+  {
+    $logger = new Logger\Laravel;
+    if( Config::has( 'd2bdoctrine.sqllogger.level' ) )
+      $logger->setLevel( Config::get( 'd2bdoctrine.sqllogger.level' ) );
+
+    $d2em->getConnection()->getConfiguration()->setSQLLogger( $logger );
+  }
+
+  /**
+   * Set up Laravel authentication via Doctrine2 provider
+   */
+  private function setupAuth()
+  {/*
         Auth::extend( 'doctrine2bridge', function() {
             return new \Illuminate\Auth\Guard(
                 new \Doctrine2Bridge\Auth\Doctrine2UserProvider(
@@ -192,64 +207,65 @@ class Doctrine2BridgeServiceProvider extends \Illuminate\Support\ServiceProvider
                 \App::make('session.store')
             );
         });
+    */
+  }
+
+
+  /**
+   * Publish configuration file
+   */
+  private function handleConfigs() {
+    $configPath = __DIR__ . '/../config/d2bdoctrine.php';
+    $this->publishes( [ $configPath => config_path('d2bdoctrine.php') ] );
+    $this->mergeConfigFrom( $configPath, 'd2bdoctrine' );
+  }
+
+
+  /**
+   * Convert Laravel5's database configuration to something what Doctrine2's
+   * DBAL providers can use.
+   *
+   * @return array
+   */
+  private function laravelToDoctrineConfigMapper()
+  {
+    switch( Config::get( 'database.default' ) ) {
+      case 'mysql':
+        return [
+            'driver'   => 'pdo_mysql',
+            'dbname'   => Config::get( 'database.connections.mysql.database' ),
+            'user'     => Config::get( 'database.connections.mysql.username' ),
+            'password' => Config::get( 'database.connections.mysql.password' ),
+            'host'     => Config::get( 'database.connections.mysql.host'     ),
+            'charset'  => Config::get( 'database.connections.mysql.charset'  ),
+            'prefix'   => Config::get( 'database.connections.mysql.prefix'   ),
+        ];
+        break;
+
+      case 'pgsql':
+        return [
+            'driver'   => 'pdo_pgsql',
+            'dbname'   => Config::get( 'database.connections.pgsql.database' ),
+            'user'     => Config::get( 'database.connections.pgsql.username' ),
+            'password' => Config::get( 'database.connections.pgsql.password' ),
+            'host'     => Config::get( 'database.connections.pgsql.host'     ),
+            'charset'  => Config::get( 'database.connections.pgsql.charset'  ),
+            'prefix'   => Config::get( 'database.connections.pgsql.prefix'   ),
+        ];
+        break;
+
+      case 'sqlite':
+        return [
+            'driver'   => 'pdo_sqlite',
+            'path'     => Config::get( 'database.connections.sqlite.database' ),
+            'user'     => Config::get( 'database.connections.sqlite.username' ),
+            'password' => Config::get( 'database.connections.sqlite.password' ),
+            'prefix'   => Config::get( 'database.connections.sqlite.prefix'   ),
+        ];
+        break;
+
+      default:
+        throw new Doctrine2Bridge\Exception\ImplementationNotFound( Config::get( 'database.default' ) );
     }
-
-
-    /**
-     * Publish configuration file
-     */
-    private function handleConfigs() {
-        $configPath = __DIR__ . '/../config/d2bdoctrine.php';
-        $this->publishes( [ $configPath => config_path('d2bdoctrine.php') ] );
-        $this->mergeConfigFrom( $configPath, 'd2bdoctrine' );
-    }
-
-
-    /**
-     * Convert Laravel5's database configuration to something what Doctrine2's
-     * DBAL providers can use.
-     *
-     * @return array
-     */
-    private function laravelToDoctrineConfigMapper()
-    {
-        switch( Config::get( 'database.default' ) ) {
-            case 'mysql':
-                return [
-                    'driver'   => 'pdo_mysql',
-                    'dbname'   => Config::get( 'database.connections.mysql.database' ),
-                    'user'     => Config::get( 'database.connections.mysql.username' ),
-                    'password' => Config::get( 'database.connections.mysql.password' ),
-                    'host'     => Config::get( 'database.connections.mysql.host'     ),
-                    'charset'  => Config::get( 'database.connections.mysql.charset'  ),
-                    'prefix'   => Config::get( 'database.connections.mysql.prefix'   ),
-                ];
-                break;
-
-            case 'pgsql':
-                return [
-                    'driver'   => 'pdo_pgsql',
-                    'dbname'   => Config::get( 'database.connections.pgsql.database' ),
-                    'user'     => Config::get( 'database.connections.pgsql.username' ),
-                    'password' => Config::get( 'database.connections.pgsql.password' ),
-                    'host'     => Config::get( 'database.connections.pgsql.host'     ),
-                    'charset'  => Config::get( 'database.connections.pgsql.charset'  ),
-                    'prefix'   => Config::get( 'database.connections.pgsql.prefix'   ),
-                ];
-                break;
-
-            case 'sqlite':
-                return [
-                    'driver'   => 'pdo_sqlite',
-                    'path'     => Config::get( 'database.connections.sqlite.database' ),
-                    'user'     => Config::get( 'database.connections.sqlite.username' ),
-                    'password' => Config::get( 'database.connections.sqlite.password' ),
-                    'prefix'   => Config::get( 'database.connections.sqlite.prefix'   ),
-                ];
-                break;
-
-                default:
-                    throw new Doctrine2Bridge\Exception\ImplementationNotFound( Config::get( 'database.default' ) );
-        }
-    }
+  }
 }
--- src/Doctrine2Bridge/Doctrine2CacheBridgeServiceProvider.php
+++ src/Doctrine2Bridge/Doctrine2CacheBridgeServiceProvider.php
@@ -5,6 +5,7 @@ namespace Doctrine2Bridge;
 use Doctrine\Common\Cache\Cache;
 
 use Config;
+use Illuminate\Support\Facades\Redis;
 
 /**
  * Doctrine2 Bridge - Brings Doctrine2 to Laravel 5.
@@ -16,137 +17,147 @@ use Config;
 class Doctrine2CacheBridgeServiceProvider extends \Illuminate\Support\ServiceProvider
 {
 
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
+  /**
+   * Bootstrap the application events.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    // handle the publishing of configuration file
+    $this->handleConfigs();
+  }
+
+
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    $this->registerCache();
+
+    // Shortcut so developers don't need to add an Alias in app/config/app.php
+    \App::booting( function() {
+      $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+      $loader->alias( 'D2Cache', 'Doctrine2Bridge\Support\Facades\Doctrine2Cache' );
+    });
+
+  }
+
+
+  private function registerCache()
+  {
+    $this->app->singleton( Cache::class, function ($app) {
+
+      $config = $this->laravelToDoctrineConfigMapper();
+
+      $cacheClass = "\Doctrine\Common\Cache\\" . $config['type'];
+
+      if(!class_exists($cacheClass))
+        throw new Exception\ImplementationNotFound( $cacheClass );
+
+      switch( $config['type'] )
+      {
+        case 'ArrayCache':
+          $cache = new $cacheClass();
+          break;
+
+        case 'FilesystemCache':
+          $cache = new $cacheClass( $config['dir'] );
+          break;
+
+        case 'MemcacheCache':
+          $cache = new $cacheClass;
+          $cache->setMemcache( $this->setupMemcache( $config ) );
+          break;
+
+        case "PredisCache":
+          $cache = new $cacheClass(Redis::connection($config['connection']));
+          break;
+      }
+
+      if( Config::has( 'd2bcache.namespace') )
+        $cache->setNamespace( Config::get( 'd2bcache.namespace') );
+
+      return $cache;
+    });
+  }
+
+  /**
+   * Setup the standard PHP memcache object implementation
+   */
+  private function setupMemcache( $config )
+  {
+    $memcache = new \Memcache;
+
+    if( !isset( $config['servers'] ) || !count( $config['servers'] ) )
+      throw new Exception\Configuration( 'No servers defined for Memcache in config/cache.php' );
+
+    foreach( $config['servers'] as $server )
     {
-        // handle the publishing of configuration file
-        $this->handleConfigs();
+      $memcache->addServer(
+          $server['host'],
+          isset( $server['port'] )         ? $server['port']         : 11211,
+          isset( $server['persistent'] )   ? $server['persistent']   : false,
+          isset( $server['weight'] )       ? $server['weight']       : 1,
+          isset( $server['timeout'] )      ? $server['timeout']      : 1,
+          isset( $server['retry_int'] )    ? $server['retry_int']    : 15
+      );
     }
 
+    return $memcache;
+  }
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->registerCache();
+  /**
+   * Publish the configuration file
+   */
+  private function handleConfigs() {
+    $configPath = __DIR__ . '/../config/d2bcache.php';
+    $this->publishes( [ $configPath => config_path('d2bcache.php') ] );
+    $this->mergeConfigFrom( $configPath, 'd2bcache' );
+  }
 
-        // Shortcut so developers don't need to add an Alias in app/config/app.php
-        \App::booting( function() {
-            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-            $loader->alias( 'D2Cache', 'Doctrine2Bridge\Support\Facades\Doctrine2Cache' );
-        });
+  /**
+   * Convert Laravel5's cache configuration to something what Doctrine2's
+   * cache providers can use.
+   *
+   * @return array
+   */
+  private function laravelToDoctrineConfigMapper()
+  {
+    switch( Config::get( 'cache.default' ) ) {
+      case 'file':
+        return [
+            'type'      => 'FilesystemCache',
+            'dir'       => Config::get( 'cache.stores.file.path' ) . DIRECTORY_SEPARATOR . 'doctine2.cache'
+        ];
+        break;
+
+      case 'array':
+        return [
+            'type'      => 'ArrayCache',
+        ];
+        break;
+
+      case 'memcached':
+        return [
+            'type'      => 'MemcacheCache',
+            'servers'   => Config::get( 'cache.stores.memcached.servers' )
+        ];
+        break;
+      case 'redis':
+        return [
+            'type'      => 'PredisCache',
+            'connection'   => Config::get( 'cache.stores.redis.connection' )
+        ];
+        break;
+
+      default:
+        throw new Exception\ImplementationNotFound( Config::get( 'cache.default' ) );
 
     }
-
-
-    private function registerCache()
-    {
-        $this->app->singleton( Cache::class, function ($app) {
-
-            $config = $this->laravelToDoctrineConfigMapper();
-
-            $cacheClass = "\Doctrine\Common\Cache\\" . $config['type'];
-
-            if(!class_exists($cacheClass))
-                throw new Exception\ImplementationNotFound( $cacheClass );
-
-            switch( $config['type'] )
-            {
-                case 'ArrayCache':
-                    $cache = new $cacheClass();
-                    break;
-                    
-                case 'FilesystemCache':
-                    $cache = new $cacheClass( $config['dir'] );
-                    break;
-
-                case 'MemcacheCache':
-                    $cache = new $cacheClass;
-                    $cache->setMemcache( $this->setupMemcache( $config ) );
-                    break;
-            }
-
-            if( Config::has( 'd2bcache.namespace') )
-                $cache->setNamespace( Config::get( 'd2bcache.namespace') );
-
-            return $cache;
-        });
-    }
-
-    /**
-     * Setup the standard PHP memcache object implementation
-     */
-    private function setupMemcache( $config )
-    {
-        $memcache = new \Memcache;
-
-        if( !isset( $config['servers'] ) || !count( $config['servers'] ) )
-            throw new Exception\Configuration( 'No servers defined for Memcache in config/cache.php' );
-
-        foreach( $config['servers'] as $server )
-        {
-            $memcache->addServer(
-                $server['host'],
-                isset( $server['port'] )         ? $server['port']         : 11211,
-                isset( $server['persistent'] )   ? $server['persistent']   : false,
-                isset( $server['weight'] )       ? $server['weight']       : 1,
-                isset( $server['timeout'] )      ? $server['timeout']      : 1,
-                isset( $server['retry_int'] )    ? $server['retry_int']    : 15
-            );
-        }
-
-        return $memcache;
-    }
-
-    /**
-     * Publish the configuration file
-     */
-    private function handleConfigs() {
-        $configPath = __DIR__ . '/../config/d2bcache.php';
-        $this->publishes( [ $configPath => config_path('d2bcache.php') ] );
-        $this->mergeConfigFrom( $configPath, 'd2bcache' );
-    }
-
-    /**
-     * Convert Laravel5's cache configuration to something what Doctrine2's
-     * cache providers can use.
-     *
-     * @return array
-     */
-    private function laravelToDoctrineConfigMapper()
-    {
-        switch( Config::get( 'cache.default' ) ) {
-            case 'file':
-                return [
-                    'type'      => 'FilesystemCache',
-                    'dir'       => Config::get( 'cache.stores.file.path' ) . DIRECTORY_SEPARATOR . 'doctine2.cache'
-                ];
-                break;
-
-            case 'array':
-                return [
-                    'type'      => 'ArrayCache',
-                ];
-                break;
-
-            case 'memcached':
-                return [
-                    'type'      => 'MemcacheCache',
-                    'servers'   => Config::get( 'cache.stores.memcached.servers' )
-                ];
-                break;
-
-            default:
-                throw new Exception\ImplementationNotFound( Config::get( 'cache.default' ) );
-
-        }
-    }
+  }
 
 }
