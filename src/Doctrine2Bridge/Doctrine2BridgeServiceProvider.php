cbb6fa36fd235eba141717b4e6a7957875b0f84d
matthew-sportle <matthew@sportle.tv>
2015-11-04 12:07:29 -0800

    Adding support for built in MYSQL function calls via DoctrineExtensions in DQL

--- src/Doctrine2Bridge/Doctrine2BridgeServiceProvider.php
+++ src/Doctrine2Bridge/Doctrine2BridgeServiceProvider.php
@@ -34,7 +34,43 @@ class Doctrine2BridgeServiceProvider extends \Illuminate\Support\ServiceProvider
    */
   private $configured = true;
 
-  /**
+    /**
+     * @var array
+     */
+    protected $datetime = [
+        'DATEADD'  => Mysql\DateAdd::class,
+        'DATEDIFF' => Mysql\DateDiff::class
+    ];
+    /**
+     * @var array
+     */
+    protected $numeric = [
+        'ACOS'    => Mysql\Acos::class,
+        'ASIN'    => Mysql\Asin::class,
+        'ATAN'    => Mysql\Atan::class,
+        'ATAN2'   => Mysql\Atan2::class,
+        'COS'     => Mysql\Cos::class,
+        'COT'     => Mysql\Cot::class,
+        'DEGREES' => Mysql\Degrees::class,
+        'RADIANS' => Mysql\Radians::class,
+        'SIN'     => Mysql\Sin::class,
+        'TAN'     => Mysql\Ta::class
+    ];
+    /**
+     * @var array
+     */
+    protected $string = [
+        'CHAR_LENGTH' => Mysql\CharLength::class,
+        'CONCAT_WS'   => Mysql\ConcatWs::class,
+        'FIELD'       => Mysql\Field::class,
+        'FIND_IN_SET' => Mysql\FindInSet::class,
+        'REPLACE'     => Mysql\Replace::class,
+        'SOUNDEX'     => Mysql\Soundex::class,
+        'STR_TO_DATE' => Mysql\StrToDat::class
+    ];
+
+
+    /**
    * Bootstrap the application events.
    *
    * @return void
